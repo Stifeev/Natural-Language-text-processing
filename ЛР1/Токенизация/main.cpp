be12c696251f9df92c721b7a86e213144ab3b62f
Stifeev <55450005+Stifeev@users.noreply.github.com>
2021-10-22 02:44:53 +0300

    Add files via upload

--- ЛР1/Токенизация/main.cpp
+++ ЛР1/Токенизация/main.cpp
@@ -5,6 +5,8 @@
 #include <filesystem>
 #include <vector>
 #include <ctime>
+#include <algorithm>
+#include <omp.h>
 
 namespace fs = std::filesystem;
 using std::set;
@@ -19,8 +21,11 @@ do                                                                \
     if (!(call))                                                  \
     {                                                             \
         fwprintf(stderr, L"[ERROR] " message L"\n", __VA_ARGS__); \
-        free(buf);                                                \
-        free(tokens);                                             \
+        for (i = 0; i < OMP_NUM_THREADS; i++)                     \
+        {                                                         \
+            free(buf[i]);                                         \
+            free(tokens[i]);                                      \
+        }                                                         \
         return 0;                                                 \
     }                                                             \
 }                                                                 \
@@ -47,20 +52,10 @@ while(0);
 // Начальный размер буфера
 #define BUF_SIZE 50000 
 
+// Количество потоков
+#define OMP_NUM_THREADS 4
+
 set<std::wstring> EXTENSIONS = { L".json", L".jsonlines", L".txt", L".xml" };
-
-bool is_letter(wchar_t c)
-{
-    c = towlower(c);
-    return L'a' <= c && c <= L'z' ||
-           L'а' <= c && c <= L'я' ||
-           c == L'ё';
-}
-
-bool is_number(wchar_t c)
-{
-    return L'0' <= c && c <= L'9';
-}
 
 int get_tokens(const wchar_t *str,
                wchar_t *tokens, int *tokens_size)
@@ -72,8 +67,8 @@ int get_tokens(const wchar_t *str,
 
     for (i = 0, j = 0; str[i] != L'\0'; i++)
     {
-        if (is_letter(str[i]) || is_number(str[i]) ||
-            (!end && str[i] == L'-' && (is_letter(str[i+1]) || is_number(str[i+1])))
+        if (iswalnum(str[i]) ||
+            (!end && str[i] == L'-' && iswalnum(str[i + 1]))
            )
         {
             tokens[j] = str[i];
@@ -82,7 +77,7 @@ int get_tokens(const wchar_t *str,
         }
         else if(!end) // Разделитель найден
         {
-            tokens[j] = ' ';
+            tokens[j] = L' ';
             j++;
             end = true;
             n_tokens++;
@@ -98,16 +93,17 @@ int wmain(int argc, wchar_t *argv[])
 {
     setlocale(LC_ALL, "Russian");
 
-    wchar_t *buf=NULL, *new_buf, *tokens=NULL;
+    wchar_t *buf[OMP_NUM_THREADS], *tokens[OMP_NUM_THREADS], *new_buf;
     int i, 
-        n_tokens, buf_size=BUF_SIZE, offset, len,
+        n_tokens, buf_size[OMP_NUM_THREADS], offset, len,
         tokens_size;
     
-    double total_tokens=0, avg_tokens=0,
-           total_size=0; 
+    double total_tokens[OMP_NUM_THREADS], avg_tokens[OMP_NUM_THREADS],
+           total_size=0;
     FILE *fpi=NULL, *fpo=NULL;
     path path2corpus = L".", path2tokenscorpus = L"./Токены", 
          path2json, path2tokens;
+    vector<std::pair<path, int>> pathes_pairs;
     vector<path> pathes;
 
     high_resolution_clock::time_point time_start, time_end;
@@ -134,82 +130,129 @@ int wmain(int argc, wchar_t *argv[])
         }
     }
 
-    ERROR_HANDLE(buf = (wchar_t *)malloc(buf_size * sizeof(wchar_t)), "Not enough memory");
-    ERROR_HANDLE(tokens = (wchar_t *)malloc(buf_size * sizeof(wchar_t)), "Not enough memory");
+    /* Выделение памяти */
+    for (i = 0; i < OMP_NUM_THREADS; i++)
+    {
+        buf[i] = NULL;
+        tokens[i] = NULL;
+        buf_size[i] = BUF_SIZE;
 
+        total_tokens[i] = 0;
+        avg_tokens[i] = 0;
+
+        ERROR_HANDLE(buf[i] = (wchar_t *)malloc(buf_size[i] * sizeof(wchar_t)), "Not enough memory");
+        ERROR_HANDLE(tokens[i] = (wchar_t *)malloc(buf_size[i] * sizeof(wchar_t)), "Not enough memory");
+    }
+
+    /* Сбор путей */
     for (auto iter : fs::recursive_directory_iterator(path2corpus))
     {
         if (iter.is_regular_file() && EXTENSIONS.find(iter.path().extension()) != EXTENSIONS.end())
         {
-            pathes.push_back(iter.path());
+            pathes_pairs.push_back({ iter.path(), iter.file_size() });
         }
+    }
+
+    auto comp = [](const std::pair<path, int> &l, const std::pair<path, int> &r) -> bool
+    {
+        return l.second < r.second;
+    };
+
+    std::sort(pathes_pairs.begin(), pathes_pairs.end(), comp);
+
+    pathes.resize(pathes_pairs.size());
+
+    for (i = 0; i < pathes_pairs.size(); i++)
+    {
+        pathes[i] = pathes_pairs[i].first;
+        total_size += pathes_pairs[i].second;
     }
 
     time_start = high_resolution_clock::now();
 
-    for (i = 0; i < pathes.size(); i++)
+    path *pathes_ptr = pathes.data();
+    int n_pathes = pathes.size();
+    #pragma omp parallel num_threads(OMP_NUM_THREADS)                                   \
+                         shared(buf, tokens, buf_size, pathes_ptr,                      \
+                                total_tokens, avg_tokens)                               \
+                         private(path2json, path2tokens, fpi, fpo, i, len, offset,      \
+                                 new_buf, n_tokens, tokens_size)                        \
+                         firstprivate(n_pathes, path2tokenscorpus)
     {
-        path2json = pathes[i];
-        path2tokens = path2tokenscorpus / (pathes[i].stem().wstring() + L"_tokens.txt");
+        int id = omp_get_thread_num(),
+            id_offset = OMP_NUM_THREADS;
 
-        WARNING_HANDLE(fpo = _wfopen(path2tokens.c_str(), L"wt, ccs=UTF-8"), "Can't create %s", path2tokens.c_str());
-        WARNING_HANDLE(fpi = _wfopen(path2json.c_str(), L"rt, ccs=UTF-8"), "Can't open %s", path2json.c_str());
-
-        INFO_HANDLE("Processing %d / %d : %s",
-                    i + 1, pathes.size(),
-                    path2json.c_str());
-        
-        offset = 0;
-        while (fgetws(buf + offset, buf_size - offset, fpi) != NULL)
+        for (i = id; i < n_pathes; i += id_offset)
         {
-            len = wcslen(buf);
+            path2json = pathes_ptr[i];
+            path2tokens = path2tokenscorpus / (pathes_ptr[i].stem().wstring() + L"_tokens.txt");
 
-            if ((len + 1) == buf_size && buf[len - 1] != L'\n') // overflow
-            {
-                buf_size *= 2;
+            WARNING_HANDLE(fpo = _wfopen(path2tokens.c_str(), L"wt, ccs=UTF-8"), "Can't create %s", path2tokens.c_str());
+            WARNING_HANDLE(fpi = _wfopen(path2json.c_str(), L"rt, ccs=UTF-8"), "Can't open %s", path2json.c_str());
 
-                INFO_HANDLE("Reallocate memory for buffer");
-
-                ERROR_HANDLE(new_buf = (wchar_t *)realloc(buf, buf_size * sizeof(wchar_t)), "Not enough memory");
-                buf = new_buf;
-                ERROR_HANDLE(new_buf = (wchar_t *)realloc(tokens, buf_size * sizeof(wchar_t)), "Not enough memory");
-                tokens = new_buf;
-
-                offset = len;
-                continue;
-            }
+            INFO_HANDLE("Thread %d processing %d/%d : %s",
+                        id, i + 1, n_pathes,
+                        path2json.c_str());
 
             offset = 0;
-
-            total_size += len;
-            n_tokens = get_tokens(buf, tokens, &tokens_size);
-            if (n_tokens > 0)
+            while (fgetws(buf[id] + offset, buf_size[id] - offset, fpi) != NULL)
             {
-                fwprintf(fpo, L"%d %d ", n_tokens, tokens_size); // Пишем количество токенов и размер буфера (в wchar'ах) для считывания (без учёта конца строки)
-                fputws(tokens, fpo);
-                fputwc(L'\n', fpo);
-            }
-            total_tokens += n_tokens;
-            avg_tokens += (double)tokens_size - n_tokens + 1;
-        }
+                len = wcslen(buf[id]);
 
-        if (fpo) fclose(fpo);
-        if (fpi) fclose(fpi);
+                if ((len + 1) == buf_size[id] && buf[id][len - 1] != L'\n') // overflow
+                {
+                    buf_size[id] *= 2;
+
+                    INFO_HANDLE("Reallocate memory for buffer in thread %d", id);
+
+                    new_buf = (wchar_t *)realloc(buf[id], buf_size[id] * sizeof(wchar_t));
+                    buf[id] = new_buf;
+                    new_buf = (wchar_t *)realloc(tokens[id], buf_size[id] * sizeof(wchar_t));
+                    tokens[id] = new_buf;
+
+                    offset = len;
+                    continue;
+                }
+
+                offset = 0;
+
+                n_tokens = get_tokens(buf[id], tokens[id], &tokens_size);
+                if (n_tokens > 0)
+                {
+                    fwprintf(fpo, L"%d %d ", n_tokens, tokens_size); // Пишем количество токенов и размер буфера (в wchar'ах) для считывания (без учёта конца строки)
+                    fputws(tokens[id], fpo);
+                    fputwc(L'\n', fpo);
+                }
+                total_tokens[id] += n_tokens;
+                avg_tokens[id] += (double)tokens_size - n_tokens + 1;
+            }
+
+            if (fpo) fclose(fpo);
+            if (fpi) fclose(fpi);
+        }
     }
 
-    total_size *= sizeof(wchar_t); // Переводим в байты
+    for (i = 1; i < OMP_NUM_THREADS; i++)
+    {
+        total_tokens[0] += total_tokens[i];
+        avg_tokens[0] += avg_tokens[i];
+    }
+
     time_end = high_resolution_clock::now();
     time_span = std::chrono::duration_cast<milliseconds>(time_end - time_start);
 
     INFO_HANDLE("Total tokens = %.0lf, avg_token = %.2lf\n"         \
                 "Total time = %.1lf sec, total size = %.3lf Gb\n"   \
                 "Speed = %.3lf ms / Kb",
-                total_tokens, avg_tokens / total_tokens,
+                total_tokens[0], avg_tokens[0] / total_tokens[0],
                 (double)time_span.count() / 1e3 , total_size / (1024. * 1024. * 1024.), 
                 (double)time_span.count() / total_size * 1024. * 1024.);
-  
-    free(buf);
-    free(tokens);
-
+    
+    for (i = 0; i < OMP_NUM_THREADS; i++)
+    {
+        free(buf[i]);
+        free(tokens[i]);
+    }
+    
     return 0;
 }
